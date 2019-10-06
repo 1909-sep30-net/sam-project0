70825107728ffdb17259f25f514741fb418d5789
Hsuan-Yu Lin <o100249243@gmail.com>
2019-10-06 10:39:27 -0500

    Library and Test Project initialized

---
+++ GStoreApp/GStoreApp.Library/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GStoreApp.Library
+{
+    class Customer
+    {
+        private string _first_name;
+        private string _last_name;
+        public int Id { get; set; }
+
+
+
+    }
+}
---
+++ GStoreApp/GStoreApp.Tests/Test.cs
@@ -0,0 +1,18 @@
+using System;
+using Xunit;
+using System.Collections.Generic;
+using System.Linq;
+using GStoreApp.Library;
+using GStoreApp.ConsoleApp;
+
+namespace GStoreApp.Tests
+{
+    public class Test
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
